55d1da5961ed006140b96c14fd2956555c9fcdd7
CIOSAI <jyinteractive.tw@gmail.com>
2023-08-23 02:07:14 +0800

    attempt to configure ghpages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
+const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,10 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/CIOSAI.github.io',
+    }
 	}
 };
 
