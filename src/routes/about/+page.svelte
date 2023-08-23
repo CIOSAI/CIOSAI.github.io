debbf1b48893c8ccddf28c9d74b9af8414019d6f
CIOSAI <jyinteractive.tw@gmail.com>
2023-08-24 00:14:21 +0800

    set up tailwind

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,7 @@
-<p>I am CIOSAI, a big fan of not having to speak English</p>
+<p class="italic">I am CIOSAI, a big fan of not having to speak English</p>
+
+<style lang="postcss">
+	p {
+		color: theme(colors.slate.200);
+	}
+</style>
