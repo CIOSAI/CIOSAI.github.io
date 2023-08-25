56df5eb51bc27bb5e49f534cdaa063617abed195
CIOSAI <jyinteractive.tw@gmail.com>
2023-08-25 18:12:44 +0800

    added carousel

---
+++ src/lib/Card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let imgSrc = '';
+	let caption = '';
+	let href = './';
+	let className = '';
+	export { imgSrc, caption, className as class };
+</script>
+
+<div class={className + ' overflow-clip relative'}>
+	<a {href} target="_blank" rel="noopener noreferrer">
+		<img
+			src={imgSrc}
+			alt={caption}
+			class="absolute w-full h-full object-cover transition-transform ease-out duration-500 hover:scale-110"
+		/>
+	</a>
+
+	<div
+		class="pointer-events-none p-4 h-full relative border-8 bg-gradient-to-b from-transparent to-slate-200 dark:to-indigo-900 border-indigo-900 dark:border-slate-200"
+	>
+		<p class="text-base absolute bottom-8">{caption}</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,25 @@
+<script lang="ts">
+	import Card from '$lib/Card.svelte';
+
+	const highlights = [
+		{ url: 'http://placekitten.com/1200/1200', caption: 'Bruh Lmao' },
+		{ url: 'http://placekitten.com/400/800', caption: 'Tall' },
+		{ url: 'http://placekitten.com/300/200', caption: 'Meow :3' },
+		{ url: 'http://placekitten.com/600/400', caption: 'Me when I the uhh when the amongus' },
+		{ url: 'http://placekitten.com/600/400', caption: 'Me when I the uhh when the amongus' },
+		{ url: 'http://placekitten.com/600/400', caption: 'Me when I the uhh when the amongus' },
+		{ url: 'http://placekitten.com/600/400', caption: 'Me when I the uhh when the amongus' },
+		{ url: 'http://placekitten.com/600/400', caption: 'Me when I the uhh when the amongus' },
+		{ url: 'http://placekitten.com/600/400', caption: 'Me when I the uhh when the amongus' },
+		{ url: 'http://placekitten.com/600/400', caption: 'Me when I the uhh when the amongus' }
+	];
+</script>
+
 <h1 class="text-indigo-950 dark:text-slate-200 text-lg mx-8 italic">Hewwo!</h1>
 <p class="text-indigo-950 dark:text-slate-200 text-base">
 	This is JY from Taiwan :33 I make videogames, music, fonts, and illustrations ^w^
 </p>
-<p class="text-indigo-950 dark:text-slate-200 text-sm my-2">
+<p class="text-indigo-950 dark:text-slate-200 text-base">
 	My favorite animal is the shoebill, they look scary and dumb
 </p>
 
@@ -14,13 +31,24 @@
 	German
 </p>
 
+<h4 class="text-indigo-950 dark:text-slate-200 text-base">I've coded in:</h4>
+<p class="text-indigo-950 dark:text-slate-200 text-base mx-8">
+	Javascript/Typescript, Rust, Nim, GLSL, Java, Python
+</p>
+
 <div class="h-16" />
 
-<h4 class="text-indigo-950 dark:text-slate-200 text-base">Highlights</h4>
-<h2 class="text-indigo-950 dark:text-slate-200 text-lg mx-8">CAROUSEL PLACEHOLDER</h2>
+<h4 class="text-indigo-950 dark:text-slate-200 text-lg">Highlights:</h4>
+<h2
+	class="text-indigo-950 dark:text-slate-200 text-lg mx-16 my-8 overflow-x-scroll snap-x flex flex-row gap-4 flex-nowrap"
+>
+	{#each highlights as card}
+		<Card imgSrc={card.url} caption={card.caption} class="snap-center w-96 h-64 flex-none" />
+	{/each}
+</h2>
 
 <div class="h-16" />
 
-<h4 class="text-indigo-950 dark:text-slate-200 text-sm">
+<h4 class="text-indigo-950 dark:text-slate-200 text-base">
 	This page is made with Svelte + Tailwind
 </h4>
