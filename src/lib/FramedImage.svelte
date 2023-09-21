9488c13ff098f190741cd67190b2e92e539ce2bf
CIOSAI <jyinteractive.tw@gmail.com>
2023-09-21 16:42:26 +0800

    trying to make lozad work

---
+++ src/lib/FramedImage.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let imgSrc = '';
+	let caption = '';
+	let className = '';
+	export { imgSrc, caption, className as class };
+</script>
+
+<div class={className + ' '}>
+	<img
+		data-src={imgSrc}
+		alt={caption}
+		class="lozad object-contain border-8 border-indigo-900 dark:border-slate-200"
+	/>
+	<p class="text-sm md:text-base">{caption}</p>
+</div>
--- src/routes/illustration/+page.svelte
+++ src/routes/illustration/+page.svelte
@@ -1,1 +1,41 @@
-<p class="text-indigo-950 dark:text-slate-200 text-base">Oops! This is where funny pictures go</p>
+<script lang="ts">
+	import FramedImage from '$lib/FramedImage.svelte';
+	import { base } from '$app/paths';
+
+	const images = [
+		{
+			url: `${base}/img/illustrations/birdy 1.png`,
+			caption: 'Scrimblo Mode : An arcade platform with just 2 buttons'
+		},
+		{
+			url: `${base}/img/illustrations/choa7.png`,
+			caption: 'Caivun : A retro display font'
+		},
+		{
+			url: `${base}/img/illustrations/jelectro1 thumbnail 1.png`,
+			caption: 'Caivun : A retro display font'
+		},
+		{
+			url: `${base}/img/illustrations/jelectro1 thumbnail 1.png`,
+			caption: 'Caivun : A retro display font'
+		},
+		{
+			url: `${base}/img/illustrations/jelectro1 thumbnail 1.png`,
+			caption: 'Caivun : A retro display font'
+		},
+		{
+			url: `${base}/img/illustrations/jelectro1 thumbnail 1.png`,
+			caption: 'Caivun : A retro display font'
+		},
+		{
+			url: `${base}/img/illustrations/jelectro1 thumbnail 1.png`,
+			caption: 'Caivun : A retro display font'
+		}
+	];
+</script>
+
+<div class="flex flex-wrap justify-center gap-4">
+	{#each images as image}
+		<FramedImage imgSrc={image.url} caption={image.caption} class="w-96" />
+	{/each}
+</div>
