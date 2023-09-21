e5bca3bb0e5450fdcb57499c8dc8a8ccf51d82c3
CIOSAI <jyinteractive.tw@gmail.com>
2023-09-21 18:33:18 +0800

    deleted about page

--- src/routes/about/+page.svelte
+++
@@ -1,4 +0,0 @@
-<p class="text-indigo-950 dark:text-slate-200 text-base">
-	Oops! You're not supposed to see this 😳
-</p>
-<a class="text-indigo-950 dark:text-slate-200 text-base" href="/">click here to go back</a>
