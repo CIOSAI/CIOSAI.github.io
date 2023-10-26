fdad983bf95e033c6d22d2ab4c3ae384d387cc50
CIOSAI <jyinteractive.tw@gmail.com>
2023-10-26 15:45:57 +0800

    refined font tester

--- src/lib/FontTester.svelte
+++ src/lib/FontTester.svelte
@@ -2,12 +2,38 @@
 	let name = '';
 	let fontSource = '';
 	let href = '';
+
+	let fontSize = 48;
+
 	export { name, fontSource, href };
 </script>
 
-<div class={`font-[${fontSource}]`}>
-	<h1 class="text-lg">{name}</h1>
-	<p>a slider for fontsize</p>
-	<p>The quick brown fox jumps over a lazy dog.</p>
-	<a class="hover:italic" {href} target="_blank" rel="noopener noreferrer">Download from here</a>
+<div class={`font-[${fontSource}]` + ' p-4 border-2 border-indigo-900 dark:border-slate-200'}>
+	<h1 class="text-[6rem]">{name}</h1>
+
+	<div class="py-4 flex items-center gap-2">
+		<input
+			class="transparent h-[4px] w-1/3 cursor-pointer appearance-none border-transparent bg-indigo-200 dark:bg-slate-500 accent-indigo-900 dark:accent-slate-200"
+			type="range"
+			min="12"
+			max="72"
+			bind:value={fontSize}
+		/>
+		<p style={`font-size: ${fontSize}px;`}>{fontSize} px</p>
+	</div>
+
+	<p contenteditable="true" spellcheck="false" style={`font-size: ${fontSize}px;`}>
+		THE QUICK BROWN FOX JUMPS OVER A LAZY DOG <br /> the quick brown fox jumps over a lazy dog
+		<br /> 0123456789 <br />
+		?!,."':;@#$%& <br />
+		`~^*_-+=\|/&lt;([&#123;
+	</p>
+	<br />
+	<a
+		style={`font-size: ${fontSize}px;`}
+		class="hover:italic"
+		{href}
+		target="_blank"
+		rel="noopener noreferrer">Download from here</a
+	>
 </div>
--- src/routes/font/+page.svelte
+++ src/routes/font/+page.svelte
@@ -2,7 +2,7 @@
 	import FontTester from '$lib/FontTester.svelte';
 </script>
 
-<div class="flex flex-col gap-6 text-indigo-900 dark:text-slate-200">
+<div class="flex flex-col gap-12 text-indigo-900 dark:text-slate-200">
 	<FontTester name="Caivun" fontSource="Caivun" href="https://ciosai.itch.io/caivunfont" />
 	<FontTester
 		name="Dead Hammer"
@@ -12,7 +12,7 @@
 	<FontTester name="Indeciso" fontSource="Indeciso" href="https://indeciso-font.vercel.app/" />
 	<FontTester name="Kaukhia" fontSource="Kaukhia" href="https://ciosai.itch.io/kaukhia" />
 	<FontTester
-		name="Rollerball-1"
+		name="Rollerball_1"
 		fontSource="Rollerball-1"
 		href="https://fontstruct.com/fontstructions/show/1749858/rollerball-1-1"
 	/>
