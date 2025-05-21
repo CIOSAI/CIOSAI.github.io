205aeed5c79d83cd57b3065df806e3f79976ae66
CIOSAI <89374056+CIOSAI@users.noreply.github.com>
2025-05-21 15:38:35 +0000

    Deploying to gh-pages from @ CIOSAI/CIOSAI.github.io@12b1a1acf29ee3e7c06d82055f5d80ec58df14cb 🚀

---
+++ _app/immutable/chunks/paths.e08a821f.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_j1rmvz)==null?void 0:s.base)??"/CIOSAI.github.io";var t;const e=((t=globalThis.__sveltekit_j1rmvz)==null?void 0:t.assets)??a;export{e as a,a as b};
